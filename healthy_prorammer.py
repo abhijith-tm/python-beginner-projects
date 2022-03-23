8c7b8ebd39eed94c3361f05b6265a4bae039536d
abhijith-tm <abhijithtm20@gmail.com>
2022-03-23 14:33:13 +0530

    new
    new program

---
+++ healthy_prorammer.py
@@ -0,0 +1,29 @@
+# Remind brogrammer to drink water, streach and break for eyes
+#program will run for 9-5
+
+#drink water in every 1 hr
+#take a break for your eyes every 30 minutes
+#get up and strech rvery 45 minutes
+
+
+import datetime
+
+from pygame import mixer
+
+mixer.i
+
+
+# drink water every 1hr 
+
+now = datetime.datetime.now()
+current_time = now.strftime("%H:%M")
+print(current_time)
+
+def water():
+    if current_time == '10:00' or current_time =='11:00' or current_time =='12:00' or current_time =='13:00' or current_time =='14:00' or current_time =='15:00' or current_time =='16:00 or current_time =='17:00':
+    pass
+
+
+
+
+
